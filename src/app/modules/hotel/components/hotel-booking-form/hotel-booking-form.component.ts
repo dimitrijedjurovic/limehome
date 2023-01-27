f538b976854851976c66169724e2a4e9bf3d4c77
Dimitrije Djurovic <dimitrije.djurovic@itengine.rs>
2023-01-27 15:17:37 +0100

    Add selected hotel, close modal

--- src/app/modules/hotel/components/hotel-booking-form/hotel-booking-form.component.ts
+++ src/app/modules/hotel/components/hotel-booking-form/hotel-booking-form.component.ts
@@ -1,4 +1,8 @@
 import {Component} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+import {IHotel} from "../../../../models/hotel.model";
+import {HotelService} from "../../services/hotel.service";
+import {MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-hotel-booking-form',
@@ -6,4 +10,15 @@ import {Component} from '@angular/core';
   styleUrls: ['./hotel-booking-form.component.scss'],
 })
 export class HotelBookingFormComponent {
+  readonly selectedHotel$: BehaviorSubject<IHotel | null> = this.hotelService.selectedHotel$;
+
+  constructor(
+    private hotelService: HotelService,
+    private modalRef: MatDialogRef<HotelBookingFormComponent>,
+  ) {
+  }
+
+  closeModal() {
+    this.modalRef.close()
+  }
 }
